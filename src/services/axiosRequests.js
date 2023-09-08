dc4c1a85571aee2e76ec79b74a85035405dca5ef
IsaacMagno <isaacmagno98@gmail.com>
2023-09-08 17:05:51 -0300

    add tasks / update champ image size

--- src/services/axiosRequests.js
+++ src/services/axiosRequests.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
-// export const BASE_URL = "http://localhost:3003";
 
 export const getFiles = async () => {
   const allFiles = await axios.get(`${BASE_URL}/uploads`).then((o) => o.data);
